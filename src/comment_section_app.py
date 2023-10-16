fae61ae02062e2b61c654998ca08e4fe514c4ba3
Victor Risgaard <victorrisgaard@hotmail.com>
2023-10-16 03:30:51 +0200

    Fix bugs and make it work with the new API Larger fix, Use env var for openAI key, move api.py to main, made better constants

--- src/comment_section_app.py
+++ src/comment_section_app.py
@@ -1,15 +1,40 @@
-import glob
+import os
 import json
+import glob
 from time import strftime
-
+from pathlib import Path
 from tkinter import END
 
 import customtkinter
+import openai
 
-from api import fetch_response, get_api_key
+# Set API key at the start
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Constants
+DEFAULT_OPTIONS = ["gpt-3.5-turbo", "gpt-4"]
+DEFAULT_SIZE = (1100, 580)
+CHAT_PATTERN = "chat_*.json"
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
+
+
+def fetch_response(model, chat_history):
+    """
+    Fetch the response from OpenAI's API given the chat history.
+    """
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=chat_history,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n"],
+    )
+    return response.choices[0].message.content
 
 
 class CommentSectionApp(customtkinter.CTk):
@@ -17,6 +42,7 @@ class CommentSectionApp(customtkinter.CTk):
         super().__init__()
         self.title("Comment Section")
         self.setup_main_layout()
+        self.chat_files_combo = customtkinter.CTkComboBox(self, values=[])
 
     def setup_main_layout(self):
         self.configure_layout()
@@ -38,55 +64,68 @@ class CommentSectionApp(customtkinter.CTk):
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="ChatGPT.py", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label = customtkinter.CTkLabel(
+            self.sidebar_frame,
+            text="ChatGPT.py",
+            font=customtkinter.CTkFont(size=20, weight="bold"),
+        )
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
     def setup_user_input_box(self):
         self.entry = customtkinter.CTkEntry(self, placeholder_text="Send a message...")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        self.entry.grid(
+            row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew"
+        )
 
     def setup_model_options(self):
-        options = ["GPT-3.5-turbo", "GPT-4"]
+        options = ["gpt-3.5-turbo", "gpt-4"]
         self.combo = customtkinter.CTkComboBox(self, values=options)
         self.combo.set(options[0])
         self.combo.grid(row=0, column=3, padx=(20, 0), pady=(20, 20), sticky="ew")
 
     def setup_submit_button(self):
-        submit_button = customtkinter.CTkButton(self, border_width=2, text="Submit", command=self.on_submit_clicked)
+        submit_button = customtkinter.CTkButton(
+            self, border_width=2, text="Submit", command=self.on_submit_clicked
+        )
         submit_button.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
     def setup_comment_section(self):
         self.comment_list_box = customtkinter.CTkTextbox(self, state="disabled")
-        self.comment_list_box.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.comment_list_box.grid(
+            row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew"
+        )
         self.load_chat()
 
     def setup_new_chat_button(self):
-        new_chat_button = customtkinter.CTkButton(self,border_width=2,text="Start a new chat", command=self.start_new_chat)
+        new_chat_button = customtkinter.CTkButton(
+            self, border_width=2, text="Start a new chat", command=self.start_new_chat
+        )
         new_chat_button.grid(row=1, column=3, sticky="ew", padx=10, pady=10)
-    
+
     ## Frontend to backend
     def get_chat_history(self):
-        chat_history = []
-        for comment_text in self.comment_list_box.get("0.0", "end"):
-            role = "user" if "You:" in comment_text else "assistant"
-            content = comment_text.replace("You: ", "").replace("Assistant: ", "")
-            chat_history.append({"role": role, "content": content})
+        chat_history: list = []
+        for comment in self.comment_list_box.get("0.0", "end").splitlines():
+            if comment.startswith("You: "):
+                chat_history.append({"role": "user", "content": comment[5:]})
+            elif comment.startswith("Assistant: "):
+                chat_history.append({"role": "assistant", "content": comment[11:]})
         return chat_history
 
     def get_response(self, user_input):
-        model = self.combo.get().lower()
+        model = self.combo.get()
         chat_history = self.get_chat_history()
         chat_history.append({"role": "user", "content": user_input})
-        return fetch_response(get_api_key(), model, chat_history)
+        response_text = fetch_response(model, chat_history)
+        return response_text
 
     def on_submit_clicked(self):
         user_input = self.entry.get()
-        if user_input == "":
-            return
-        self.update_comment_list_box("You: " + user_input)
-
-        response_text = self.get_response(user_input)
-        self.update_comment_list_box("Assistant: " + response_text)
+        if user_input:
+            self.update_comment_list_box(f"You: {user_input}")
+            response_text = self.get_response(user_input)
+            self.update_comment_list_box(f"Assistant: {response_text}")
+            self.entry.delete(0, "end")
 
     def update_comment_list_box(self, text):
         self.comment_list_box.configure(state="normal")
@@ -105,11 +144,11 @@ class CommentSectionApp(customtkinter.CTk):
             json.dump(chat_data, f)
 
     def load_chat(self):
-        chat_files = sorted(glob.glob("chat_*.json"))
+        chat_files = sorted(Path().glob(CHAT_PATTERN))
         for file_name in chat_files:
-            self.chat_files_combo.add(file_name)
-        if len(chat_files) > 0:
-            self.chat_files_combo.set(chat_files[-1])
+            self.chat_files_combo.add(file_name.name)
+        if chat_files:
+            self.chat_files_combo.set(chat_files[-1].name)
             self.load_chat_file()
 
     def load_chat_file(self):
@@ -121,10 +160,12 @@ class CommentSectionApp(customtkinter.CTk):
         for comment in chat_data["comments"]:
             self.comment_list_box.insert(END, comment + "\n")
         self.comment_list_box.configure(state="disabled")
-        
+
+
 def main():
     app = CommentSectionApp()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
